bb053518635d8b4b730aae3a71760194d6be8fa1
Felixfelicia <131186447+Felixfelicia@users.noreply.github.com>
2023-05-18 22:41:36 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from aiogram.utils import executor
+import logging
+
+from config import dp, bot, ADMINS
+from handlers import commands, callback, admin, fsmAdminMentor, extra
+from database.bot_db import sql_create
+
+
+async def on_startup(dp):
+    await bot.send_message(ADMINS[1], "Подключена!")
+    sql_create()
+
+
+async def on_shutdown(dp):
+    await bot.send_message(ADMINS[1], "See you!")
+
+
+commands.register_handlers_commands(dp)
+callback.register_handlers_callback(dp)
+admin.register_handlers_admin(dp)
+fsmAdminMentor.register_handlers_fsmAdminMentor(dp)
+
+extra.register_handlers_extra(dp)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup,
+                           on_shutdown=on_shutdown)
